9c27b7ad7769a146b9a4d614d5a67689535a8359
uctzmr0 <32728660+uctzmr0@users.noreply.github.com>
2017-10-19 15:46:54 +0100

    Add files via upload

---
+++ Tutorial.py
@@ -0,0 +1,26 @@
+#Arithmetic Operators
+num1 = 10
+num2 = 20
+
+print "num1 + num2 =", num1+num2
+print"num1 - num2 =", num1-num2
+print"num1 * num2 =", num1*num2
+print"num1 / num2 =", float(num1/num2)
+print"5^3 =", 5**3
+print"20%3=", 20%3
+print"22//7=", float(22//7)
+
+#Assignment Operators
+num3 = num1+num2
+print 'num3=', num3
+# num 3+= num2 -------> num3 = num3 + num2
+num3+= num2
+print'num3=', num3
+
+#Comparison Operators
+print "is num3 > num2", num3 > num2
+print "is num2 = num3", num2 == num3
+print "is num1 != num2", num1 != num2
+
+
+#https://www.youtube.com/watch?v=N0lxfilGfak
